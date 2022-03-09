de3ca78a513e9f4523e809350311d2392e2061de
nakamura minami <35647616+kagunyan25@users.noreply.github.com>
2022-03-09 11:47:01 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -45,3 +45,18 @@
 // val5[0] = 'bird';
 // val5.push('hamster');
 // console.log(val5);
+
+// /**
+//  * テンプレート文字列
+//  */
+// const name = '美波';
+// const age = '23';
+// // 「私の名前は美波です。年齢は23歳です。」
+
+// // 従来の方法
+// const message1 = '私の名前は' + name + 'です。年齢は' + age +'歳です。';
+// console.log(message1);
+
+// // テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
