97eaface7dce8eb8a6e89edadf3eb1b32c0377c5
nakamura minami <35647616+kagunyan25@users.noreply.github.com>
2022-03-15 14:30:35 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -109,5 +109,44 @@
 /**
  * デフォルト値、引数など
  */
-const sayhello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`);
-sayhello("美波");
+// const sayhello = (name = "ゲスト") => console.log(`こんにちは${name}さん！`);
+// sayhello("美波");
+
+/**
+ * スプレッド構文 ...
+ */
+//　 配列の展開
+// const arr1 = [1, 2];
+// // console.log(arr1); // => [1, 2]
+// // console.log(...arr1); // => 1 2
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1]); // => 3
+// sumFunc(...arr1); // => 3
+
+// // まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...num3] = arr2;
+// console.log(num1); // => 1
+// console.log(num2); // => 2
+// console.log(num3); // => [3, 4, 5]
+
+// // 配列のコピー、結合
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6); // => [10, 20]
+
+// arr6[0] = 100;
+// console.log(arr6); // => [100, 20]
+// console.log(arr4); // => [10, 20]
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7); // => [10, 20, 30, 40]
+
+// // ※= でコピーしたものに代入すると元の配列にも変化が生じてしまう
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log (arr6); // => [100, 20]
+// console.log (arr4); // => [100, 20]
